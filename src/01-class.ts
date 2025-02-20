410ff4e290d13426fe0eb5e4621ffa4d63a9d094
lgonzaloquijada <lgonzalo.quijada@gmail.com>
2025-02-19 21:14:52 -0700

    chore: methods.

--- src/01-class.ts
+++ src/01-class.ts
@@ -8,7 +8,7 @@ console.log(date.toUTCString());
 const date2 = new Date(1993, 10, 18); // 0 is January and 11 is December
 console.log(date2.toDateString());
 
-class MyDate {
+export class MyDate {
   year: number;
   month: number;
   day: number;
@@ -22,3 +22,13 @@ class MyDate {
 
 const myDate = new MyDate(1993, 10, 18);
 console.log(myDate.year);
+
+enum Weekday {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
---
+++ src/02-methods.ts
@@ -0,0 +1,34 @@
+export class MyDate {
+  year: number;
+  month: number;
+  day: number;
+
+  constructor(year: number, month: number, day: number) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+
+  printFormat(): string {
+    return `${this.year}/${this.month}/${this.day}`;
+  }
+
+  add(amount: number, type: 'days' | 'months' | 'years') {
+    switch (type) {
+      case 'days':
+        this.day += amount;
+        break;
+      case 'months':
+        this.month += amount;
+        break;
+      case 'years':
+        this.year += amount;
+        break;
+    }
+  }
+}
+
+const myDate = new MyDate(2021, 1, 1);
+console.log(myDate.printFormat()); // 2021/1/1
+myDate.add(3, 'days');
+console.log(myDate.printFormat()); // 2021/1/2
