e037c2ae190517a483d33d2df485910ed2ce8c3d
lgonzaloquijada <lgonzalo.quijada@gmail.com>
2025-02-19 21:04:41 -0700

    chore: first class

---
+++ src/01-class.ts
@@ -0,0 +1,24 @@
+const date = new Date();
+console.log(date.getHours());
+console.log(date.toDateString());
+console.log(date.toTimeString());
+console.log(date.toISOString());
+console.log(date.toUTCString());
+
+const date2 = new Date(1993, 10, 18); // 0 is January and 11 is December
+console.log(date2.toDateString());
+
+class MyDate {
+  year: number;
+  month: number;
+  day: number;
+
+  constructor(year: number, month: number, day: number) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+}
+
+const myDate = new MyDate(1993, 10, 18);
+console.log(myDate.year);
