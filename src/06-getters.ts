7bd39008d821469ebc181b110c53eeed508e77c1
lgonzaloquijada <lgonzalo.quijada@gmail.com>
2025-02-21 23:23:59 -0700

    chore: getters

---
+++ src/06-getters.ts
@@ -0,0 +1,51 @@
+export class MyDate {
+  constructor(
+    public year: number = 1993,
+    public month: number = 7,
+    private _day: number = 9
+  ) {}
+
+  public printFormat(): string {
+    const day = this.addPadding(this.day);
+    const month = this.addPadding(this.month);
+    return `${this.year}/${month}/${day}`;
+  }
+
+  get day() {
+    return this._day;
+  }
+
+  isLeapYear(): boolean {
+    return (
+      this.year % 4 === 0 && (this.year % 100 !== 0 || this.year % 400 === 0)
+    );
+  }
+
+  private addPadding(value: number): string {
+    return value < 10 ? `0${value}` : `${value}`;
+  }
+
+  public add(amount: number, type: 'days' | 'months' | 'years') {
+    switch (type) {
+      case 'days':
+        this._day += amount;
+        break;
+      case 'months':
+        this.month += amount;
+        break;
+      case 'years':
+        this.year += amount;
+        break;
+    }
+  }
+
+  getDay() {
+    return this.day;
+  }
+}
+
+const myDate = new MyDate(2021, 1, 1);
+console.log(myDate.day); // 1
+//myDate.day = 2;
+//console.log(myDate.day); // 2
+console.log(myDate.getDay()); // 1
