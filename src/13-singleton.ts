370af475b4082d904e19c633b6a8d2cfe78cf955
lgonzaloquijada <lgonzalo.quijada@gmail.com>
2025-02-24 21:06:35 -0700

    chore: singleton with TS.

---
+++ src/13-singleton.ts
@@ -0,0 +1,25 @@
+export class MyService {
+  static instance: MyService | null = null;
+
+  private constructor(private name: string) {}
+
+  getName() {
+    return this.name;
+  }
+
+  static create(name: string) {
+    if (MyService.instance === null) {
+      MyService.instance = new MyService(name);
+    }
+
+    return MyService.instance;
+  }
+}
+
+const myService1 = MyService.create('Service 1');
+console.log(myService1.getName());
+
+const myService2 = MyService.create('Service 2');
+console.log(myService2.getName());
+
+console.log(myService1 === myService2); // false
