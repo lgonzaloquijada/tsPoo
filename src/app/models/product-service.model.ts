5d182e43dbabb41fef5c4f16dfc314baed2e0b40
lgonzaloquijada <lgonzalo.quijada@gmail.com>
2025-02-25 21:51:46 -0700

    feat: adding ProductHttpService.

---
+++ src/app/models/product-service.model.ts
@@ -0,0 +1,14 @@
+import { CreateProductDto, UpdateProductDto } from '../dtos/product.dto';
+import { Product } from './product.model';
+
+export interface ProductService {
+  getAll(): Product[] | Promise<Product[]>;
+  create(data: CreateProductDto): Product | Promise<Product>;
+  update(
+    id: Product['id'],
+    changes: UpdateProductDto
+  ): Product | Promise<Product>;
+  findOne(
+    id: Product['id']
+  ): Product | undefined | Promise<Product | undefined>;
+}
